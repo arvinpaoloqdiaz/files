3952401bf9843f1a2c87f483a0ad1fb4abd7d231
arvin.diaz <arvin.diaz@searix.net>
2025-08-20 10:37:22 +0800

    ADD json files

---
+++ HardSkills.js
@@ -0,0 +1,63 @@
+const HardSkills = [
+	{
+		title:"Revision Control Systems",
+		entries:[
+		"Git Basics",
+		"GitHub",
+		"GitLab",
+		"Gitea"
+		]
+	},
+	{
+		title:"Frontend Web Development",
+		entries:[
+		"HTML5",
+		"CSS3",
+		"Bootstrap",
+		"Wireframes",
+		"Prototyping",
+		"Mockups",
+		"PHP"
+		]
+	},
+	{
+		title:"Backend Development",
+		entries:[
+		"JavaScript",
+		"Node.js",
+		"MongoDB",
+		"Express.js",
+		"Postman",
+		"REST API"
+		]
+	},
+	{
+		title:"Full-stack Development",
+		entries:[
+		"React.js",
+		"HTML DOM",
+		"API w/ Fetch",
+		"SDLC",
+		"Trello",
+		"Laravel"
+		]
+	},
+	{
+		title:"Data Analysis & Visualization",
+		entries:[
+		"MatLab",
+		"R Language",
+		"SQL",
+		"Python"
+		]
+	},
+	{
+		title:"Unit Testing",
+		entries:[
+		"Mocha",
+		"Chai",
+		"Chai-http"
+		]
+	}
+]
+export default HardSkills;
